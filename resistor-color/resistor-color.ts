65df4b0e873167d5d405a75329e605cd6d293d3e
Beau Nelford <nelfordbc@gmail.com>
2021-07-19 17:42:03 -0600

    adding resistor color

---
+++ resistor-color/resistor-color.ts
@@ -0,0 +1,5 @@
+export const colorCode = (code:string) => {
+  return COLORS.indexOf(code);
+}
+
+export const COLORS = ['black','brown','red','orange','yellow','green','blue','violet','grey','white'];
